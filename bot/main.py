462658c2cff3064881bd802a6602db8a60e6a754
Marc <marctheemperor@gmail.com>
2022-12-04 09:58:42 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,19 @@
-import discord
 import os
-from discord.ext import commands
+import discord
+from discord import app_commands
 
-
-bot = commands.Bot(command_prefix = "!")
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-@bot.slash_command(name="runnit")
-async def first_slash(ctx): 
-    await ctx.respond("You executed the slash command! High Five!")
+@tree.command(name = "sup", description = "My first application Command", guild=discord.Object(id=292068243579994113)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def first_command(interaction):
+    await interaction.response.send_message("Nice!")
 
-bot.run(TOKEN)
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=292068243579994113))
+    print("Ready!")
+    
+client.run(TOKEN)
