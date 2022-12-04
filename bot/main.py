2747f3f3ff4c7243a76824b48c9941ef9e423bc8
Marc <marctheemperor@gmail.com>
2022-12-04 10:02:19 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,19 @@
 import os
 import discord
-from discord import app_commands
+import discord.ext
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-@tree.command(name = "sup", description = "My first application Command", guild=discord.Object(id=292068243579994113)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-async def first_command(interaction):
-    await interaction.response.send_message("Nice!")
-
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=292068243579994113))
     print("Ready!")
+    
+@tree.command(name = "sup", description = "My first application Command", guild=discord.Object(id=292068243579994113)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def slashing_commanding(int: discord.Interaction):    
+    await int.response.send_message("sup dude")
     
 client.run(TOKEN)
