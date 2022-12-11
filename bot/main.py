bd2e6c2e78b5a5d55586b95e8a07b68377151c19
Marc <marctheemperor@gmail.com>
2022-12-11 01:02:26 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot()
+bot = commands.Bot("mh!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.slash_command(name="first_slash", guild_ids=[292068243579994113]) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
