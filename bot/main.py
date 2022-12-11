2327a2c722e3fbfc0a941c977f9954462bb9864c
Marc <marctheemperor@gmail.com>
2022-12-11 01:00:22 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,9 +2,10 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.slash_command(name="first_slash", guild_ids=[292068243579994113]) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_slash(ctx): 
     await ctx.respond("You executed the slash command!")
     
-bot.run("Mzk0MTY3NDg2MDEyMTk0ODI4.GuPKC5.icln1tAO15PYVzrvsnpUTdmgoY6krkijEuzY_Y")
+bot.run(TOKEN)
